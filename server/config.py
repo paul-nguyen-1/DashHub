4db66a021194ad50c64d7e3c2792798fd1a949fb
paul-nguyen96 <paul-nguyen-1@users.noreply.github.com>
2026-03-30 16:20:29 -0500

    update attribute

--- server/config.py
+++ server/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
     port: int = 8000
     allowed_origins: list[str] = ["http://localhost:3000"]
